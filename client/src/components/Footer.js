8fa1ed167afe87b3012712a535ffb81a6e2792f5
Av720 <120581801+Av720@users.noreply.github.com>
2023-04-11 22:32:06 -0600

    added footer contents and links

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <div>
+            <footer id="footer">
+                <h3>Contact me</h3>
+                <div className="icons-div"> 
+                    <a href="https://www.instagram.com/aldov__/"><i className="icons fa-brands fa-instagram fa-xl"></i></a>
+                    <a href="https://www.facebook.com/aldo.huertavalencia"><i className="icons fa-brands fa-facebook fa-xl"></i></a>
+                    <a href="https://github.com/Av720"><i className="icons fa-brands fa-github fa-xl"></i></a>
+                    <a href="https://www.linkedin.com/in/aldo-valencia-95b250237/"><i className="icons fa-brands fa-linkedin fa-xl"></i></a>
+                </div>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer;
