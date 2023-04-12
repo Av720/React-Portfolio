0c8bf39bd8bfb7ce1d9efb409601abeda427b061
Av720 <120581801+Av720@users.noreply.github.com>
2023-04-11 22:31:35 -0600

    added starter code

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-function About () {
+function About() {
+    return (
+        <div
+       
+            className=""
+        >
+            <div className="">
+                <div className="pb-8">
+                    <p className="">
+                        About
+                    </p>
+                </div>
 
+                <p className="text-xl mt-5">
+                    
+                </p>
+
+                <br />
+
+                <p className="">
+                    
+                </p>
+            </div>
+        </div>
+    );
 };
 
 
